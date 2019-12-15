f810c9eb66acd2089eab8c4689d82ed47d3dd867
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-12-15 23:44:08 +0500

    chore: Resolve ts issues

--- src/components/AboutSettingsList/AboutSettingsList.tsx
+++ src/components/AboutSettingsList/AboutSettingsList.tsx
@@ -14,63 +14,54 @@ export interface AboutSettingsListProps {
 }
 
 export class AboutSettingsList extends React.PureComponent<AboutSettingsListProps> {
+	static contextType: React.Context<BlueBase> = BlueBaseContext;
 
-			static contextType = BlueBaseContext;
-
-			static defaultStyles = (theme: Theme) => ({
-			iconRight: {
+	static defaultStyles = (theme: Theme) => ({
+		iconRight: {
 			color: theme.palette.text.disabled,
 			transform: [{ scaleX: I18nManager.isRTL ? -1 : 1 }],
 		},
-		})
+	});
 
-			renderVersion = ({ __ }: IntlContextData) => {
-			const BB: BlueBase = this.context;
-			const version = BB.Configs.getValue('version');
+	renderVersion = ({ __ }: IntlContextData) => {
+		const BB: BlueBase = this.context;
+		const version = BB.Configs.getValue('version');
 
-			return version && <List.Item title={__('Version')} description={__(version)} />;
-		}
+		return version && <List.Item title={__('Version')} description={__(version)} />;
+	};
 
-			renderAuthor = ({ __ }: IntlContextData) => {
-			const BB: BlueBase = this.context;
-			const { styles = {} } = this.props;
+	renderAuthor = ({ __ }: IntlContextData) => {
+		const BB: BlueBase = this.context;
+		const { styles = {} } = this.props;
 
-			const author = BB.Configs.getValue('author');
-			const authorUrl = BB.Configs.getValue('authorUrl');
+		const author = BB.Configs.getValue('author');
+		const authorUrl = BB.Configs.getValue('authorUrl');
 
 		// If there is not author, return null
-			if (!author) {
+		if (!author) {
 			return null;
 		}
 
-			const props: any = {
+		const props: any = {
 			description: __(author),
 			title: __('Developed by'),
 		};
 
 		// If there is not authorUrl, only render name
-			if (!authorUrl) {
+		if (!authorUrl) {
 			return <List.Item {...props} />;
 		}
 
-			props.right = <Icon name="open-in-new" style={styles.iconRight} />;
+		props.right = <Icon name="open-in-new" style={styles.iconRight} />;
 
+		return <ExternalLink Component={List.Item} props={props} href={authorUrl} />;
+	};
 
-			return (
-			<ExternalLink Component={List.Item} props={props} href={authorUrl} />
-		);
-		}
-
-			render() {
-
-			return (
+	render() {
+		return (
 			<IntlConsumer>
-				{(intl) => {
-
-					const items = [
-						this.renderVersion(intl),
-						this.renderAuthor(intl),
-					]
+				{intl => {
+					const items = [this.renderVersion(intl), this.renderAuthor(intl)]
 						// Remove undefined items
 						.filter(x => !!x);
 
@@ -79,7 +70,7 @@ export class AboutSettingsList extends React.PureComponent<AboutSettingsListProp
 							{items.map((item, index) => (
 								<React.Fragment key={index}>
 									{item}
-									{(index < items.length - 1) ? <Divider /> : null}
+									{index < items.length - 1 ? <Divider /> : null}
 								</React.Fragment>
 							))}
 						</View>
@@ -87,5 +78,5 @@ export class AboutSettingsList extends React.PureComponent<AboutSettingsListProp
 				}}
 			</IntlConsumer>
 		);
-		}
+	}
 }
--- src/components/LocalizationSettingsList/LocalizationSettingsList.tsx
+++ src/components/LocalizationSettingsList/LocalizationSettingsList.tsx
@@ -1,36 +1,38 @@
 import { BlueBase, BlueBaseContext, getComponent } from '@bluebase/core';
 import { Divider, List } from '@bluebase/components';
+
 import React from 'react';
 
 const TextDirectionSetting = getComponent('TextDirectionSetting', 'Noop');
 const LanguageSetting = getComponent('LanguageSetting', 'Noop');
 
 export class LocalizationSettingsList extends React.PureComponent {
+	static contextType: React.Context<BlueBase> = BlueBaseContext;
 
-			static contextType = BlueBaseContext;
+	render() {
+		const BB: BlueBase = this.context;
 
-			render() {
+		const hasLanguageSetting = BB.Configs.getValue(
+			'plugin.settings-app.general.language.selection'
+		);
+		const hasTextDirection = BB.Configs.getValue(
+			'plugin.settings-app.general.language.text-direction'
+		);
 
-			const BB: BlueBase = this.context;
-
-			const hasLanguageSetting = BB.Configs.getValue('plugin.settings-app.general.language.selection');
-			const hasTextDirection = BB.Configs.getValue('plugin.settings-app.general.language.text-direction');
-
-			const items = [
+		const items = [
 			hasLanguageSetting && <LanguageSetting />,
 			hasTextDirection && <TextDirectionSetting />,
-		]
-		.filter(x => !!x);
+		].filter(x => !!x);
 
-			return (
+		return (
 			<List>
 				{items.map((item, index) => (
 					<React.Fragment key={index}>
 						{item}
-						{(index < items.length - 1) ? <Divider inset /> : null}
+						{index < items.length - 1 ? <Divider inset /> : null}
 					</React.Fragment>
 				))}
 			</List>
 		);
-		}
+	}
 }
