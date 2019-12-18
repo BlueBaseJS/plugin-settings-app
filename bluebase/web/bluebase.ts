d31e81204856bebf7146ddea7401e416bc43373c
Ali Waqar <aliwaqar981@gmail.com>
2019-12-18 10:59:34 +0500

    fix(actionEnable): Updated github actions

--- bluebase/web/bluebase.ts
+++ bluebase/web/bluebase.ts
@@ -4,8 +4,8 @@ import commonBootOptions from '../common/bluebase';
 
 /**
  * Add your platform specific configs here.
- * We keep all the universal (cross platform) configs in
- * the common folder, and extend them here.
+ * We keep all the universal (cross platform) configurations in
+ * the common folder,and extend them here.
  */
 const bootOptions: Partial<BootOptions> = {};
 
