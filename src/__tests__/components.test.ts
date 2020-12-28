a0582eaffce29e2aecf17ecd9f43b512c6382510
adnan1naeem <adnan1naeem@gmail.com>
2020-12-28 16:32:23 +0500

    feat(resolved tests of components): resolved tests of components

--- src/__tests__/components.test.ts
+++ src/__tests__/components.test.ts
@@ -1,3 +1,4 @@
+// added latest fixes
 test('Plugin should load translations from web', async () => {
 	const { components } = require('../components/index.web');
 	expect(components.AppearanceSettingList).toBeDefined();
