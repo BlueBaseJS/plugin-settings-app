eb1c7e65464a3cd84d621aa9a593722978ba64d7
Abdul Rehman Talat <rehman.talat@gmail.com>
2022-05-01 14:12:46 +0500

    refactor: Removed dead code

--- src/hooks/useScreenProps.ts
+++
@@ -1,9 +0,0 @@
-import { useBlueBase, useIntl, useTheme } from '@bluebase/core';
-
-export function useScreenProps() {
-	const BB = useBlueBase();
-	const intl = useIntl();
-	const { theme } = useTheme();
-
-	return { BB, intl, theme };
-}
